36adb184bb4fc7e3cae5d5a2035f97851019a594
tavyla <tavylavitoria4@gmail.com>
2024-09-24 11:13:06 -0300

    Add files via upload

---
+++ aulaLDP.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int numero1 = 8;
+int numero2 = 20;
+int soma;
+soma = numero1 + numero2;
+char letra = '1';
+char nome [6]={"tavyla"}; 
+printf("%c",letra);
+printf("%s",nome);
+}
